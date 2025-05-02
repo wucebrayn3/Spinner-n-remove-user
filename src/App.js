97e741b6f95ed4006a23c34dc254ee2785134057
Juliannn <flbloodheist@gmail.com>
2025-05-02 22:44:12 +0800

    height change

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
     <div className="flex justify-center items-center h-screen w-full p-4 bg-no-repeat bg-cover" style={{ backgroundImage: `url("./images/landscape.jpg")` }}>
       <div className="flex flex-row gap-8 overflow-y-auto scrollbar-hide w-full items-center">
         
-        <div className="flex flex-col justify-center items-start bg-black/20 backdrop-blur-sm border border-violet/30 shadow-xl rounded-2xl p-8 w-[90vw] h-[700px] max-w-[600px]">
+        <div className="flex flex-col justify-center items-start bg-black/20 backdrop-blur-sm border border-violet/30 shadow-xl rounded-2xl p-8 w-[90vw] h-[500px] max-w-[600px]">
 
           <UserForm />
         </div>
         
         
-        <div className="flex flex-col items-center bg-black/20 backdrop-blur-sm border border-white/30- shadow-slate-800/50 rounded-xl p-8 w-[90vw] h-[700px] max-w-[600px]">
+        <div className="flex flex-col items-center bg-black/20 backdrop-blur-sm border border-white/30- shadow-slate-800/50 rounded-xl p-8 w-[90vw] h-[500px] max-w-[600px]">
           <h2 className="text-2xl text-black font-semibold text-slate-100 mb-4 border-b border-black pb-2 w-full text-center">
             Fetched Users
           </h2>
@@ -25,7 +25,7 @@ function App() {
         </div>
         
         
-        <div className="flex flex-col items-center bg-black/20 backdrop-blur-sm border border-white/30 shadow-slate-800/50 rounded-xl p-8 w-[90vw] h-[700px] max-w-[600px]">
+        <div className="flex flex-col items-center bg-black/20 backdrop-blur-sm border border-white/30 shadow-slate-800/50 rounded-xl p-8 w-[90vw] h-[500px] max-w-[600px]">
           <h2 className="text-2xl text-black font-semibold text-slate-100 mb-4 border-b border-black pb-2 w-full text-center">
             Manually Added Users
           </h2>
